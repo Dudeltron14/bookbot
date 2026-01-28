8522edb0f499dd28b723ca9aa7f10faa76294ae1
Noah Wittman <dudeltron14@gmail.com>
2026-01-27 16:27:36 -0800

    refactored code, completed the character count function in stats, added notes & added character count print statement to main function.

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from stats import word_count, character_count
 
 def main():
+    #sets book path and makes book text into a string
     book_path = "books/frankenstein.txt"
     book_text = get_book_text(f"{book_path}")
+    
+    #sets word count variable and prints it
     num_words = word_count(book_text)
     print(f"Found {num_words} total words")
+
+    #print statement to print the character count dictionary
+    print(f"Character count is {character_count(book_text)}")
 
 #function to open selected book and provide it's contents as a string
 def get_book_text(book_filepath):
--- stats.py
+++ stats.py
@@ -5,5 +5,14 @@ def word_count(book_contents):
 
 #function to take text from the book as a string and returns the number of times each character, (iincluding symbols and spaces), appears in the string.
 def character_count(book_text):
-    book_text.lower()
+    lowercase_text = book_text.lower()
+    character_dict = {
+        
+    }
+    for character in lowercase_text:
+        if character in character_dict:
+            character_dict[character] += 1
+        else:
+            character_dict[character] = 1
     
+    return character_dict
